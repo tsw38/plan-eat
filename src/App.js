537e83f614acb6e875f26974a9dbbf8b637eaac7
Tyler Williams <tyler.williams@macys.com>
2019-02-26 22:03:30 -0600

    Initial CRA setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <StyledP>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </StyledP>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -25,4 +24,11 @@ class App extends Component {
   }
 }
 
+const StyledP = styled.p`
+    font-size:20px;
+    color:tomato;
+`;
+
 export default App;
+
+
