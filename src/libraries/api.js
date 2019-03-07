486f4c65a0a6897fab62cedc010c3a4684cad220
Tyler Williams <tyler.williams@footlocker.com>
2019-03-07 17:35:56 -0600

    More progress to signin

--- src/libraries/api.js
+++ src/libraries/api.js
@@ -1,6 +1,22 @@
+import { jsonToGraphQLQuery } from 'json-to-graphql-query';
+import axios from 'axios';
+console.log('this is the process', process.env.API_URL);
 // all the graphql api stuff will go here
 
-export const api = (stuffz) => (dispatch, getState, options) => {
+export const api = (dispatch, getState, options) => {
+    console.warn(dispatch, getState, options);
+
+    // return axios({
+    //     method: 'post',
+    //     url: process.env.API_URL,
+    //     data: query
+    // }).then(resp => {
+    //     console.warn('this is the options', options);
+    //     console.warn('this is the resp', resp);
+    //     return resp;
+    // }).finally(resp => {
+    //     console.warn('this most likely broke', resp);
+    // })
     return new Promise((resolve, reject) => {
         console.warn('awdawdawdawdawdawd')
         setTimeout(() => {
