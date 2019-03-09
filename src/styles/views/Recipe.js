37974b950ad8866785ca9e3ba7972adde9c673d4
Tyler Williams <tyler.williams@macys.com>
2019-03-09 15:01:32 -0600

    Recipe - Left side

--- src/styles/views/Recipe.js
+++ src/styles/views/Recipe.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
+import {transition, colorButtons} from 'styles/mixins/index';
+import {darken, lighten, parseToHsl, rgbToColorString} from 'polished';
+
 import * as sizing from 'styles/sizing';
 import * as colors from 'styles/colors';
 import * as variables from 'styles/variables';
@@ -10,6 +13,24 @@ export const StyledRecipe = styled.div`
         grid-template-columns: calc(50% - ${sizing.spacingxs}) auto;
         width: 100%;
         grid-gap: ${sizing.spacingmd};
+
+        ul, ol {
+            margin-left: 1rem;
+        }
+
+        ol {
+            list-style: decimal;
+            list-style-position: inside;
+        }
+
+        li {
+            margin-bottom: 1rem;
+            line-height: 1.25;
+        }
+
+        .row {
+            display: grid;
+        }
 
         .column {
             width: 100%;
@@ -22,16 +43,30 @@ export const StyledRecipe = styled.div`
             }
 
             &:last-of-type {
-                background-color: ${colors.stormPetrel};
+            }
+        }
 
+        .Button {
+            .Icon--Wrapper {
+                height: ${sizing.spacingmdl};
+
+                svg {
+                    width: auto;
+                }
             }
         }
     }
 
     .Recipe {
+
         &--Header {
+            margin-bottom: 1.5rem;
+
+            .Button {
+                border: none;
+                padding: ${sizing.spacing3xs};
+            }
             .row {
-                display: grid;
                 grid-template-columns: auto auto;
                 align-items: center;
             }
@@ -59,19 +94,64 @@ export const StyledRecipe = styled.div`
             display: grid;
             grid-template-columns: repeat(5, auto);
             margin: 0 0 0 auto;
+        }
+
+        &--Section {
+            &:not(:last-of-type) {
+                border-bottom: 1px solid ${colors.ballerina};
+            }
+
+            .row {
+                grid-template-columns: auto auto;
+                align-items: center;
+            }
+
+            .Recipe--Ingredient-List {
+                color: ${colors.stormPetrel};
+            }
+        }
+
+        &--Serving-Toggle {
+            display: grid;
+            grid-template-columns: repeat(2,auto);
+            margin: 0 0 0 auto;
 
             .Button {
-                border: none;
-                padding: ${sizing.spacing3xs};
+                border-radius: 0;
+                border: ${sizing.spacing3xs} solid ${colors.imperialPrimer};
+                padding: 0.25rem;
 
-                .Icon--Wrapper {
-                    height: ${sizing.spacingmdl};
+                :first-of-type {
+                    fill: ${colors.imperialPrimer};
 
-                    svg {
-                        width: auto;
+                    &:hover {
+                        ${colorButtons(colors.imperialPrimer)}
+                        fill: white;
                     }
                 }
+
+                :last-of-type {
+                    ${colorButtons(colors.imperialPrimer)}
+                    fill: white;
+
+                    &:hover {
+                        ${colorButtons(darken(0.2, colors.imperialPrimer))}
+                        fill: white;
+                    }
+                }
+
+                .Icon--Wrapper {
+                    height: 1rem;
+                }
             }
+        }
+
+        &--SectionTitle {
+            font-weight: ${variables.fontLight};
+            font-size: ${sizing.spacingmdl};
+            margin-left: 0;
+            margin-right: 0;
+            margin-bottom:1.5rem;
         }
 
         &--Image {
@@ -81,9 +161,13 @@ export const StyledRecipe = styled.div`
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
+            background-color: ${colors.stormPetrel};
         }
     }
 `;
 
 export const RecipeHeader = styled.div``;
-export const RecipeIngredients = styled.div``;
+export const RecipeSection = styled.div`
+    margin-bottom: 1rem;
+
+`;
--- src/views/Recipe/Recipe.jsx
+++ src/views/Recipe/Recipe.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyledRecipe, RecipeHeader, RecipeIngredients } from 'styles/views/Recipe';
+import { StyledRecipe, RecipeHeader, RecipeSection } from 'styles/views/Recipe';
 
 import Row from 'common/Layout/Row';
 import Link from "common/Link/Link";
@@ -37,23 +37,61 @@ class Recipe extends React.Component {
                         </Row>
                     </RecipeHeader>
 
-                    <RecipeIngredients
-                        className="Recipe--IngredientsWrapper">
+                    <RecipeSection
+                        className="Recipe--Section">
                         <Row>
-                            <h3 className="Recipe--Ingredients"></h3>
+                            <h3 className="Recipe--SectionTitle">Ingredients</h3>
                             <div className="Recipe--Serving-Toggle">
-
+                                <Button iconName={'minus'} />
+                                <Button iconName={'plus'} />
                             </div>
                         </Row>
+                        <ul className="Recipe--Ingredient-List">
+                            <li>4 avocados, halved and pitted</li>
+                            <li>2 cooked chicken breasts, shredded</li>
+                            <li>4 ounces cream cheese, softened</li>
+                            <li>1/4 cup chopped tomatoes</li>
+                            <li>1/4 teaspoon salt</li>
+                            <li>1/4 teaspoon ground black pepper</li>
+                            <li>1 pinch cayenne pepper</li>
+                            <li>1/2 cup shredded Parmesan cheese, or more to taste</li>
+                            <li>Add additional Ingredients</li>
+                        </ul>
+                    </RecipeSection>
+                    <RecipeSection
+                        className="Recipe--Section">
                         <Row>
-                            <ul className="Recipe--Ingredient-List"></ul>
+                            <h3 className="Recipe--SectionTitle">Directions</h3>
                         </Row>
-                    </RecipeIngredients>
+                        <ol className="Recipe--Ingredient-List">
+                            <li>
+                                Preheat oven to 400 degrees F (200 degrees C).
+                            </li>
+                            <li>
+                                Scoop out some of the flesh in the center of each avocado; place into  mixing bowl. Add chicken, cream cheese, tomatoes, salt, pepper, and  cayenne pepper; mix well to combine. Scoop spoonfuls of chicken  mixture into the wells of each avocado; top each with generous amount  of Parmesan cheese. Place avocado halves, face-up, in muffin cups to  stabilize.
+                            </li>
+                            <li>
+                                Bake avocados in preheated oven until cheese is melted, 8 to 10 minutes.
+                            </li>
+                        </ol>
+                    </RecipeSection>
+
+                    <RecipeSection
+                        className="Recipe--Section">
+                        <Row>
+                            <h3 className="Recipe--SectionTitle">Chef's Notes</h3>
+                        </Row>
+                        <ul className="Recipe--Ingredient-List">
+                            <li>
+                                Bacon ipsum dolor amet pork belly bacon short ribs, ham turkey frankfurter beef drumstick short loin picanha ham hock fatback. Ham hock kielbasa tail, shoulder flank picanha tri-tip beef prosciutto doner kevin. Filet mignon leberkas fatback kielbasa turkey. Drumstick andouille frankfurter brisket cupim spare ribs pastrami kielbasa buffalo ground round.
+                            </li>
+                        </ul>
+                    </RecipeSection>
                 </Column>
                 <Column>
                     <div
                         className="Recipe--Image"
-                        style={{backgroundImage: 'url(https://images.unsplash.com/photo-1552057008-518700a52ad0?ixlib=rb-1.2.1&auto=format&fit=crop&w=802&q=80)'}}
+                        style={{backgroundImage: 'url(https://images.unsplash.com/photo-1529268127899-36bf4524c254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)'}}
                     />
                     <div className="Recipe--Time"></div>
                     <div className="Recipe--Nutrition"></div>
