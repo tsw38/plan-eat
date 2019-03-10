a5ec6c3b930a2feb05743268ef4521c3f52677e3
Tyler Williams <tyler.williams@macys.com>
2019-03-10 01:08:56 -0600

    Recipe Page Added

--- src/views/Recipe/Recipe.jsx
+++ src/views/Recipe/Recipe.jsx
@@ -19,6 +19,10 @@ import * as colors from 'styles/colors';
 // import { userInfo } from "os";
 
 class Recipe extends React.Component {
+    state = {
+        servingSize: 4
+    }
+
     componentDidMount() {
         const {
             network,
@@ -52,13 +56,15 @@ class Recipe extends React.Component {
             ingredients: storedIngredients
         } = this.props;
 
+        const {servingSize} = this.state;
+
         if (!thisRecipe) {
             return null;
         }
 
         const uploadedBy = network[thisRecipe.uploadedBy] || '';
 
-        const ingredients = storedIngredients && thisRecipe.ingredients.reduce((temp, ingredient) => {
+        const ingredients = !!Object.keys(storedIngredients).length && thisRecipe.ingredients.reduce((temp, ingredient) => {
             const mergedIngredient = {
                 ...ingredient,
                 ...storedIngredients[ingredient.id]
@@ -66,7 +72,6 @@ class Recipe extends React.Component {
 
             if (mergedIngredient.name) {
                 const converted = convertUnit(mergedIngredient.quantity, mergedIngredient.unit, true);
-                // console.warn(converted);
 
                 return [
                     ...temp,
@@ -79,6 +84,32 @@ class Recipe extends React.Component {
             return temp;
         }, []);
 
+        const nutrition = !!Object.keys(storedIngredients).length && !!thisRecipe.ingredients && thisRecipe.ingredients.map(ingred => {
+            const quantity = ingred.quantity/100;
+
+            const {calories, protein, fat, carbs} = storedIngredients[ingred.id].nutrition;
+
+            return {
+                calories: Math.ceil(calories*quantity),
+                protein:  Math.ceil(protein*quantity),
+                fat: 	  Math.ceil(fat*quantity),
+                carbs:	  Math.ceil(carbs.absolute*quantity),
+                fiber: 	  Math.ceil(carbs.dietaryFiber*quantity),
+                sugar: 	  Math.ceil(carbs.sugar*quantity)
+            };
+        }).reduce((finalCalcs, ingredientNuts) => {
+            return {
+                ...finalCalcs,
+                calories:Math.ceil(ingredientNuts.calories/this.state.servingSize + (finalCalcs.calories || 0)),
+                protein: Math.ceil(ingredientNuts.protein/this.state.servingSize + (finalCalcs.protein || 0)),
+                fat: 	 Math.ceil(ingredientNuts.fat/this.state.servingSize + (finalCalcs.fat || 0)),
+                carbs:	 Math.ceil(ingredientNuts.carbs/this.state.servingSize + (finalCalcs.carbs || 0)),
+                fiber: 	 Math.ceil(ingredientNuts.fiber/this.state.servingSize + (finalCalcs.fiber || 0)),
+                sugar: 	 Math.ceil(ingredientNuts.sugar/this.state.servingSize + (finalCalcs.sugar || 0))
+            }
+        }, {});
+
+        // console.warn(nutrition);
 
         return (
             <StyledRecipe className="Recipe">
@@ -170,31 +201,31 @@ class Recipe extends React.Component {
                         </div>
                     </div>
                     <div className="Recipe--Nutrition">
-                        <h3 className="Recipe--Section--Title">Nutrition Facts</h3>
+                        <h3 className="Recipe--Section--Title">Nutrition Facts (per serving)</h3>
                         <div className="Recipe--Nutrition--Table">
                             <p className="Calories">
                                 <span className="category">Calories</span>
-                                <span className="value">450</span>
+                                <span className="value">{nutrition.calories}</span>
                             </p>
                             <p className="Sugar">
                                 <span className="category">Sugar</span>
-                                <span className="value">20g</span>
+                                <span className="value">{nutrition.sugar}</span>
                             </p>
                             <p className="Protein">
                                 <span className="category">Protein</span>
-                                <span className="value">30g</span>
+                                <span className="value">{nutrition.protein}</span>
                             </p>
                             <p className="Fiber">
                                 <span className="category">Fiber</span>
-                                <span className="value">55g</span>
+                                <span className="value">{nutrition.fiber}</span>
                             </p>
                             <p className="Fat">
                                 <span className="category">Fat</span>
-                                <span className="value">30g</span>
+                                <span className="value">{nutrition.fat}</span>
                             </p>
                             <p className="Carbs">
                                 <span className="category">Carbs</span>
-                                <span className="value">10g</span>
+                                <span className="value">{nutrition.carbs}</span>
                             </p>
                         </div>
                     </div>
